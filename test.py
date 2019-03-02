d1fabb97f4e5df3535e657d0d45bef9eb7d273fa
defyhf <defyhf@gmail.com>
2019-03-03 05:46:48 +0800

    Added Download Timeline And a few more tests

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
-
-from zenki import Setup
+import json
+import pprint
+from zenki import Setup, Zenki, Downloader, Console
 
 class TestSetup:
   @classmethod
@@ -11,18 +12,43 @@ class TestSetup:
       subprocess.call([sys.executable, "-m", "pip", "uninstall", "-y",
                        package], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
   
-class ZenkiSetupTest(unittest.TestCase):
+  
+class ZenkiASetupTest(unittest.TestCase):
   def setUp(self):
+    Console.testMode = True
     TestSetup.removePackages()
+    with open('config.test.json') as f:
+      self.config = json.loads(f.read())
+      self.config['CreateClientSecret'] = False
   
   def test_setup(self):
-    self.assertEqual(Setup.setup(), 0, 'Setup install Failed')
+    pprint.pprint(self.config)
+    self.assertEqual(Setup.setup(**self.config), 'OK', 'Setup install Failed')
     import mastodon, requests
     self.assertEqual(mastodon.__name__, 'mastodon', 'Mastodon not installed correctly.')
     self.assertEqual(requests.__name__, 'requests', 'Requests not installed correctly.')
 
-  def tearDown(self):
-    TestSetup.removePackages()
+
+class ZenkiBClientTest(unittest.TestCase):
+  def setUp(self):
+    Console.testMode = True
+    with open('config.test.json') as f:
+      self.testConfig = json.loads(f.read())
+      self.testConfig.pop('password')
+    self.ZenkiInstance = Zenki.loadInstanceFromConfig()
+
+  def test_Acreate_client(self):
+    self.assertEqual(self.ZenkiInstance.config, self.testConfig)
+  
+  def test_Bdownload_post(self):
+    post_id = 101682564527694969
+    post = self.ZenkiInstance.mclient.status(post_id)
+    self.ZenkiInstance.downloader.downloadMediaStatus(post)
+
+  def test_Cdownload_multi_post(self):
+    post_id = 100493658352114533
+    post = self.ZenkiInstance.mclient.status(post_id)
+    self.ZenkiInstance.downloader.downloadMediaStatus(post)
 
 
 if __name__ == '__main__':
--- zenki.py
+++ zenki.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os, shutil, json, threading, queue
 
 LOG_LEVEL = 0
 
@@ -10,6 +10,8 @@ class Console():
       2 = Log
       3 = None
   """
+  testMode = False
+
   @classmethod
   def log(self, *args, **kwargs):
     if LOG_LEVEL <= 2:
@@ -30,11 +32,15 @@ class Console():
     if LOG_LEVEL <= 2:
       print('[!!]', *args, **kwargs)
 
+  @classmethod
+  def testlog(self, *args, **kwargs):
+    if self.testMode:
+      print('[TEST]', *args, **kwargs)
 
 class Setup():
   
   @classmethod
-  def setup(self):
+  def setup(self, **custom):
     self.printLogo()
     Console.log('No configuration found, running setup.')
     Console.log('---- Inital Stuff ----')
@@ -43,26 +49,40 @@ class Setup():
     Console.ok('Python Version')
     Console.warn('Verifying needed modules...')
     self.checkAndInstallRequirements()
-    self.createCredentials()
-    return 0
+    self.createCredentials(custom)
+    return 'OK'
 
   @classmethod
   def createCredentials(self, custom={}):
-    import json
+    _base_url, _email, _password =  "", "", ""
+    if 'base_url' not in custom:
+      _base_url = input("Instance URL ('https://pawoo.net'): ")
+      _email = input("Email: ")
+      _password = input("Password (visible): ")
+    
     config = {
-      'base_url': custom.get('api_base_url', input("Instance URL ('https://pawoo.net'): ")),
+      'base_url': custom.get('base_url', _base_url),
       'app_secret': custom.get('app_secret', 'zenki.app.secret'),
       'user_secret': custom.get('user_secret', 'zenki.user.secret'),
-      'email': custom.get('email', input("Email: ")),
-      'password': custom.get('password', input("Password (visible): ")),
+      'email': custom.get('email', _email),
+      'password': custom.get('password', _password),
+      'download': {
+          'save_path': custom.get('save_path', './'),
+          'folder_if_multiple': custom.get('folder_if_multiple', True),
+          'folder_per_user': custom.get('folder_per_user', True),
+          'user_folder_format': custom.get('user_folder_format', '{account.id}-{account.acct}'),
+          'media_filename_format': custom.get('media_filename_format', '{media.id}-{raw}.{extension}')
+      }
     }
 
     from mastodon import Mastodon
-    Console.log('Creating Client Secret...')
-    Mastodon.create_app('zenki',
-      api_base_url=config['base_url'],
-      to_file = config['app_secret']
-    )
+
+    if custom.get('CreateClientSecret', True):
+      Console.log('Creating Client Secret...')
+      Mastodon.create_app('zenki',
+        api_base_url=config['base_url'],
+        to_file = config['app_secret']
+      )
     mastodon = Mastodon(
         client_id=config['app_secret'],
         api_base_url=config['base_url'],
@@ -75,7 +95,7 @@ class Setup():
     )
     config.pop('password')
     Console.log('Writing Config...')
-    with open('config.json', 'w') as f:
+    with open(custom.get('configpath', 'config.json'), 'w') as f:
       f.write(json.dumps(config, indent=4))
     Console.ok('Credential Setup Done!')
 
@@ -103,4 +123,150 @@ class Setup():
                         stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         Console.log('Module:', module, package, 'installed.')
 
+
+class DownloadWorker(threading.Thread):
+
+  def __init__(self, workerIndex, r, workQueue):
+    threading.Thread.__init__(self)
+    self.daemon = True
+    self.index = workerIndex
+    self.queue = workQueue
+    self.r = r
+
+  def run(self):
+    while True:
+      url, path = self.queue.get()
+      Console.log('[ZenkiWorker-{index}] Saving: {url}'.format(index=self.index, url=url))
+      response = self.r.get(url, stream=True)
+      with open(path, 'wb') as out_file:
+          shutil.copyfileobj(response.raw, out_file)
+      del response
+      self.queue.task_done()
+
+
+class Downloader():
+
+  def __init__(self, config):
+    import requests
+    self.r = requests
+    self.queue = queue.Queue(config.get('queue_size', 10))
+    self.__dict__.update({
+      'save_path'         : config.get('save_path', './'),
+      'folder_if_multiple': config.get('folder_if_multiple', True),
+      'folder_per_user'   : config.get('folder_per_user', True),
+      'user_folder_format': config.get('user_folder_format', '{account.id}-{account.acct}'),
+      'media_filename_format': config.get('media_filename_format', '{media.id}-{raw}.{extension}')
+    })
+
+    self.workers = [DownloadWorker(x, self.r, self.queue)
+                    for x in range(config.get('worker_size', 5))]
+    for i in self.workers:
+      i.start()
+  
+
+  def createPath(self, post):
+    final_path = [self.save_path]
+    if self.folder_per_user:
+      final_path.append(self.user_folder_format.format(account=post.account))
+    if self.folder_if_multiple and len(post.media_attachments) > 1:
+      final_path.append(str(post.id))
+
+    Console.testlog(final_path)
+    directory = os.path.join(*final_path)
+    os.makedirs(directory, exist_ok=True)
+    return final_path
+
+
+  def downloadMediaStatus(self, post):
+    for media in post.media_attachments:
+      path = self.createPath(post)
+      url_filename = media.url.split('/')[-1]
+      raw_filename = ".".join(url_filename.split('.')[:-1])
+      extension = media.url.split('.')[-1]
+      filename = self.media_filename_format.format(media=media, raw=raw_filename, extension=extension)
+      path.append(filename)
+      file_path = os.path.join(*path)
+      
+      Console.testlog('Queueing: ', file_path)
+      self.queue.put((media.url, file_path))
+      # response = self.r.get(media.url, stream=True)
+      # with open(file_path, 'wb') as out_file:
+      #     shutil.copyfileobj(response.raw, out_file)
+      # del response
+    
+    return 'OK'
+
+    
+
+class Zenki():
+  
+  @classmethod
+  def loadInstanceFromConfig(self, configJson='config.json'):
+    Console.log('Loading From Json: ', configJson)
+    import json
+    #try:
+    with open(configJson) as f:
+      config = json.loads(f.read())
+      return Zenki(**config)
+    #except Exception as e:
+    #  Console.error('Loading Json Failed: ', e)
+
+
+  def __init__(self, **config):
+    from mastodon import Mastodon
+    self.config = config
+    self.downloader = Downloader(config['download'])
+    self.mclient = Mastodon(
+      access_token=config['user_secret'],
+      client_secret=config['app_secret'],
+      api_base_url=config['base_url']
+    )
+    self.resolved_users = {}
+
+  def resolveUserId(self, username):
+    if username == int:
+      return username
+    if username not in self.resolved_users:
+      result = self.mclient.account_search(username)
+      if result:
+        self.resolved_users[username] = result[0]
+      else:
+        try:
+          return int(username)
+        except:
+          pass
+        raise NoUserFound(username)
+    
+    return self.resolved_users[username].id
+
+
+  def downloadTimelineImages(self, username):
+    userId = self.resolveUserId(username)
+    Console.log('Downloading Timeline:', userId)
+    timeline = self.mclient.account_statuses(userId, only_media=True)
+    while timeline:
+      for status in timeline:
+        Console.log('Queueing', status.id)
+        self.downloader.downloadMediaStatus(status)
+      timeline = self.mclient.fetch_next(timeline)
+    Console.log('Waiting for the remaining downloads to finish.')
+    self.downloader.queue.join()
+
+
+class NoUserFound(Exception):
+  pass
+
+
+if __name__ == "__main__":
+  config_path = os.path.join(os.path.dirname(sys.argv[0]), 'config.json')
+  if not os.path.exists(config_path):
+    Setup.setup(configpath=config_path)
+    
+  zenki = Zenki.loadInstanceFromConfig(config_path)
+  arguments = sys.argv[1:]
+  op = arguments.pop(0)
+  
+  if op == 'downloadtimeline':
+    username = arguments.pop(0)
+    zenki.downloadTimelineImages(username)
 
